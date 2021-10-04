c85b198db0a7b32e432d40d59084388acd8067ba
loser_just_developing <joshtheman55555@gmail.com>
2021-10-03 23:46:11 -0400

    Started snake

---
+++ snake.py
@@ -0,0 +1,19 @@
+import turtle as t
+import os
+import random
+
+WIDTH, HEIGHT = 600, 600
+
+wn = t.Screen()
+wn.title("Snake")
+wn.bgcolor("black")
+wn.setup(width=WIDTH, height=HEIGHT)
+wn.tracer(0)
+
+
+
+
+
+
+
+wn.mainloop()
