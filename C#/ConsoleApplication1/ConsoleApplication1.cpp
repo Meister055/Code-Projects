ceebc3b58106d40c534dcdf64c15637d8f8884f1
Joshua S <joshtheman55555@gmail.com>
2022-10-14 13:13:00 -0400

    stuff ig

---
+++ C#/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	char* color;
+	printf("Type a color: \n");
+	scanf_s("%c", color);
+
+	printf("%c", color);
+
+}
---
+++ C#/Cs Stuff/Completed Programs/MadLib.cs
@@ -0,0 +1,46 @@
+﻿
+// Pick adjective
+string color = "";
+Console.WriteLine("Pick a color: ");
+color = Console.ReadLine();
+
+// Pick Superlative
+string superlative = "";
+Console.WriteLine("Pick a superlative(ending in est): ");
+superlative = Console.ReadLine();
+
+string adjective1 = "";
+Console.WriteLine("Pick an adjective: ");
+adjective1 = Console.ReadLine();
+
+string body_part = "";
+Console.WriteLine("Pick a body part(plural): ");
+body_part = Console.ReadLine();
+
+string body_part2 = "";
+Console.WriteLine("Pick a body part: ");
+body_part2 = Console.ReadLine();
+
+string noun = "";
+Console.WriteLine("Pick a noun: ");
+noun = Console.ReadLine();
+
+string animal = "";
+Console.WriteLine("Pick a animal(plural): ");
+animal = Console.ReadLine();
+
+string adjective2 = "";
+Console.WriteLine("Pick a different adjective: ");
+adjective2 = Console.ReadLine();
+
+string adjective3 = "";
+Console.WriteLine("Pick a different adjective: ");
+adjective3 = Console.ReadLine();
+
+string adjective4 = "";
+Console.WriteLine("Pick a different adjective: ");
+adjective4 = Console.ReadLine();
+
+Console.WriteLine("The " + color + " Dragon is the " + superlative + " Dragon of all. It has " + adjective1 + " " + body_part + ", and a " + body_part2 +  
+    " shaped like a " + noun + ". It loves to eat " + animal + ", although it will feast on nearly anything. It is " + adjective2 + " and " + adjective3 + ". " +
+    "You must be " + adjective4 + " " +"around it, or you may end up as it`s meal!");
